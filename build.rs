925450dd3bd6722469e699b6cec865a10fe28701
Simon Walker <s.r.walker101@googlemail.com>
2019-11-02 02:10:17 +0000

    Remove commented out code in build file

--- build.rs
+++ build.rs
@@ -1,27 +1,10 @@
 use std::path::PathBuf;
-// use walkdir::WalkDir;
 
 fn main() {
-    // let protos: Vec<_> = WalkDir::new("protos")
-    //     .into_iter()
-    //     .filter_map(Result::ok)
-    //     .map(|e| e.into_path())
-    //     .filter(|p| {
-    //         // Get only the files that are proto files
-    //         let ext = p.extension();
-    //         match ext {
-    //             Some(ext) => ext == "proto",
-    //             None => false,
-    //         }
-    //     })
-    //     .collect();
-    let protos = &[
-        // "protos/tensorflow_serving/apis/classification.proto",
-        "protos/tensorflow_serving/apis/prediction_service.proto",
-    ]
-    .iter()
-    .map(PathBuf::from)
-    .collect::<Vec<_>>();
+    let protos = &["protos/tensorflow_serving/apis/prediction_service.proto"]
+        .iter()
+        .map(PathBuf::from)
+        .collect::<Vec<_>>();
 
     match tonic_build::configure().compile(&protos, &[PathBuf::from("protos")]) {
         Ok(_) => {}
